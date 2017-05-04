19b278ca3404d0212028e97c5ea716265630bb2d
Dan Korostelev <nadako@gmail.com>
2017-05-04 18:59:27 +0300

    update and document StackFrame

--- protocol/debug/Types.hx
+++ protocol/debug/Types.hx
@@ -960,14 +960,62 @@ typedef Source = {
     @:optional var adapterData:Dynamic;
 }
 
+@:enum
+abstract StackFramePresentationHint(String) to String {
+    var normal = 'normal';
+    var label = 'label';
+}
+
+/**
+    A Stackframe contains the source location.
+**/
 typedef StackFrame = {
+    /**
+        An identifier for the stack frame. It must be unique across all threads.
+        This id can be used to retrieve the scopes of the frame with the 'scopesRequest' or to restart the execution of a stackframe.
+    **/
     var id:Int;
+
+    /**
+        The name of the stack frame, typically a method name.
+    **/
     var name:String;
+
+    /**
+        The optional source of the frame.
+    **/
     @:optional var source:Source;
+
+    /**
+        The line within the file of the frame. If source is null or doesn't exist, line is 0 and must be ignored.
+    **/
     var line:Int;
+
+    /**
+        The column within the line. If source is null or doesn't exist, column is 0 and must be ignored.
+    **/
     var column:Int;
+
+    /**
+        An optional end line of the range covered by the stack frame.
+    **/
     @:optional var endLine:Int;
+
+    /**
+        An optional end column of the range covered by the stack frame.
+    **/
     @:optional var endColumn:Int;
+
+    /**
+        The module associated with this frame, if any.
+    **/
+    @:optional var moduleId:EitherType<Int,String>;
+
+    /**
+        An optional hint for how to present this frame in the UI.
+        A value of 'label' can be used to indicate that the frame is an artificial frame that is used as a visual label or separator.
+    **/
+    @:optional var presentationHint:StackFramePresentationHint;
 }
 
 typedef Scope = {
