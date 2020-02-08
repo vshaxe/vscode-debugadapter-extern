a4884d5536b0621aad50d18923cb313842a9a1e8
Jens Fischer <jensfischer95@gmail.com>
2020-02-08 19:27:31 +0100

    Update to vscode-debugadapter-node 1.38.0
    https://github.com/microsoft/vscode-debugadapter-node/compare/01fa3d9...5b37a18

--- src/vscode/debugAdapter/Protocol.hx
+++ src/vscode/debugAdapter/Protocol.hx
@@ -2,9 +2,24 @@ package vscode.debugAdapter;
 
 import vscode.debugProtocol.DebugProtocol;
 
+typedef DebugProtocolMessage = {}
+
+@:jsRequire("vscode-debugadapter", "Disposable0")
+extern class Disposable0 {
+	function dispose():Any;
+}
+
+typedef Event0<T> = (listener:(e:T) -> Any, ?thisArg:Any) -> Disposable0;
+
 @:jsRequire("vscode-debugadapter", "ProtocolServer")
 extern class ProtocolServer {
 	function sendEvent<T>(event:Event<T>):Void;
 	function sendResponse<T>(response:Response<T>):Void;
 	function sendRequest<T>(command:String, args:Dynamic, timeout:Float, cb:(response:Response<T>) -> Void):Void;
+
+	/** vscode.DebugAdapter methods **/
+	var onDidSendMessage(default, null):Event0<DebugProtocolMessage>;
+
+	function dispose():Any;
+	function handleMessage(message:ProtocolMessage):Void;
 }
